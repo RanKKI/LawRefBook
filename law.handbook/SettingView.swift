1e051cc6022a5b2ca7cebf36e707d54d268090b6
RanKKI <youloveonlymeh@gmail.com>
2022-02-26 15:54:37 +0800

    feat: setting sheet

--- law.handbook/SettingView.swift
+++ law.handbook/SettingView.swift
@@ -9,19 +9,20 @@ import Foundation
 import SwiftUI
 
 struct SettingView: View {
-    
-    @Environment(\.dismiss) var dismiss
-    
+
     var body: some View {
         List{
-            Section(header: Text("测试1")) {
-                Text("12321“")
+            Section(header: Text("内容来源"), footer: Text("如果您发现了任何错误，包括但不限于排版、错字、缺失内容，请使用以下联系方式告知开发者，以便修复")){
+                Text("全国人民代表大会")
+                Text("http://www.npc.gov.cn")
             }
-            Section(header: Text("测试1")) {
-                Text("12321“")
+            Section(header: Text("开发者")){
+                Text("@RanKKI")
+                Text("some.email@email.com")
             }
-            Section(header: Text("测试1")) {
-                Text("12321“")
+            Section(footer: Text("自豪地采用 SwiftUI")){
+                Text("给 App 评分！")
+                Text("[在 GitHub 上贡献](https://github.com/RanKKI/chinese.law.handbook)")
             }
         }
     }
