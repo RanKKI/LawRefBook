49eaad2d1484d1fff6a58b48d113f1f79277a588
RanKKI <youloveonlymeh@gmail.com>
2022-02-26 17:00:44 +0800

    fix: content sources

--- law.handbook/SettingView.swift
+++ law.handbook/SettingView.swift
@@ -13,8 +13,8 @@ struct SettingView: View {
     var body: some View {
         List{
             Section(header: Text("内容来源"), footer: Text("如果您发现了任何错误，包括但不限于排版、错字、缺失内容，请使用以下联系方式告知开发者，以便修复")){
-                Text("全国人民代表大会")
-                Text("http://www.npc.gov.cn")
+                Text("国家法律法规数据库")
+                Text("https://flk.npc.gov.cn")
             }
             Section(header: Text("开发者")){
                 Text("@RanKKI")
