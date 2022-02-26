d17c750e7b847088bdd7c9aa0b6c1d2166f25261
RanKKI <youloveonlymeh@gmail.com>
2022-02-26 17:14:32 +0800

    feat: 民事诉讼法以及其解释

--- law.handbook/Config.swift
+++ law.handbook/Config.swift
@@ -34,12 +34,15 @@ var laws: [LawGroup] = [
         Law(name: "国籍法"),
         Law(name: "身份证法"),
     ]),
-    LawGroup(name: "宪法修正案", laws: [
-        Law(name: "宪法修正案（1988年）", folder:"宪法修正案", file: "1988年"),
-        Law(name: "宪法修正案（1993年）", folder:"宪法修正案", file: "1993年"),
-        Law(name: "宪法修正案（1999年）", folder:"宪法修正案", file: "1999年"),
-        Law(name: "宪法修正案（2004年）", folder:"宪法修正案", file: "2004年"),
-        Law(name: "宪法修正案（2018年）", folder:"宪法修正案", file: "2018年"),
+    LawGroup(name: "民法", laws: [
+        Law(name: "消费者权益保护法"),
+        Law(name: "个人信息保护法"),
+    ]),
+    LawGroup(name: "行政法", laws: [
+        Law(name: "食品安全法"),
+    ]),
+    LawGroup(name: "诉讼与非诉讼程序法", laws: [
+        Law(name: "民事诉讼法"),
     ]),
     LawGroup(name: "民法典", laws: [
         Law(name: "总则", folder: "民法典"),
@@ -51,11 +54,14 @@ var laws: [LawGroup] = [
         Law(name: "侵权责任", folder: "民法典"),
         Law(name: "附则", folder: "民法典"),
     ]),
-    LawGroup(name: "民法", laws: [
-        Law(name: "消费者权益保护法"),
-        Law(name: "个人信息保护法"),
+    LawGroup(name: "宪法修正案", laws: [
+        Law(name: "宪法修正案（1988年）", folder:"宪法修正案", file: "1988年"),
+        Law(name: "宪法修正案（1993年）", folder:"宪法修正案", file: "1993年"),
+        Law(name: "宪法修正案（1999年）", folder:"宪法修正案", file: "1999年"),
+        Law(name: "宪法修正案（2004年）", folder:"宪法修正案", file: "2004年"),
+        Law(name: "宪法修正案（2018年）", folder:"宪法修正案", file: "2018年"),
     ]),
-    LawGroup(name: "行政法", laws: [
-        Law(name: "食品安全法"),
+    LawGroup(name: "司法解释", laws: [
+        Law(name: "最高人民法院关于适用《中华人民共和国民事诉讼法》的解释", folder: "司法解释", file: "民事诉讼法"),
     ]),
 ]
