a87da739417c73e4872f09adf0c0a84f46010a3d
RanKKI <youloveonlymeh@gmail.com>
2022-03-08 08:56:40 +0800

    feat: request review when luanch two times

--- Handbook/App.swift
+++ Handbook/App.swift
@@ -28,5 +28,13 @@ struct MainApp: App {
             UserDefaults.standard.set(curVersion, forKey: "lastVersion")
         }
     }
+    
+    private func checkRunTimes(){
+        let launchTime = UserDefaults.standard.integer(forKey: "launchTimes")
+        if launchTime == 2 {
+            AppStoreReviewManager.requestReviewIfAppropriate()
+        }
+        UserDefaults.standard.set(launchTime + 1, forKey: "launchTimes")
+    }
 
 }
