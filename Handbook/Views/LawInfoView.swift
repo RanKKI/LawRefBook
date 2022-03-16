6dcc59539a8acb4c8e54e43e8878d717c3e0424a
RanKKI <youloveonlymeh@gmail.com>
2022-03-16 13:11:31 +0800

    feat: related laws in info page

--- Handbook/Views/LawInfoView.swift
+++ Handbook/Views/LawInfoView.swift
@@ -2,27 +2,37 @@ import Foundation
 import SwiftUI
 
 struct LawInfoPage: View {
-
+    
     var lawID: UUID
-
+    
     @Environment(\.dismiss) var dismiss
-
+    
     var body: some View {
-        List(LawProvider.shared.getLawInfo(lawID), id: \.id){ info in
-            if info.header.isEmpty {
-                if let text = try? AttributedString(markdown: info.content) {
+        List{
+            ForEach(LawProvider.shared.getLawInfo(lawID), id: \.id) { info in
+                if !info.header.isEmpty {
+                    Section(header: Text(info.header)){
+                        Text(info.content)
+                            .textSelection(.enabled)
+                    }
+                } else if let text = try? AttributedString(markdown: info.content) {
                     Text(text)
                         .frame(maxWidth: .infinity, alignment: .center)
                         .multilineTextAlignment(.center)
                         .listRowSeparator(.hidden)
                         .textSelection(.enabled)
                 }
-            }else{
-                Section(header: Text(info.header)){
-                    Text(info.content)
-                        .textSelection(.enabled)
+            }
+            if let law = LocalProvider.shared.getLaw(lawID) {
+                if let arr = law.links {
+                    Section(header: Text("相关法律法规")) {
+                        ForEach(arr, id: \.self) { uid in
+                            NaviLawLink(uuid: uid)
+                        }
+                    }
                 }
             }
+            
         }
         .listStyle(.plain)
         .toolbar {
