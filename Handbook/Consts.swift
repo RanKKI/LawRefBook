01a82b3fdfbad0e55bff47b304c225bb6c006dab
RanKKI <youloveonlymeh@gmail.com>
2022-04-01 10:28:38 +0800

    feat: add contributors

--- Handbook/Consts.swift
+++ Handbook/Consts.swift
@@ -20,3 +20,22 @@ enum SearchType : String, CaseIterable {
     case catalogue = "目录"
     case fullText = "全文"
 }
+
+
+let COPYRIGHT_DECLARE = """
+根据《中华人民共和国著作权法》第五条，本作品不适用于该法。如不受其他法律、法规保护，本作品在中国大陆和其他地区属于公有领域。
+不适用于《中华人民共和国著作权法》的作品包括：
+（一）法律、法规，国家机关的决议、决定、命令和其他具有立法、行政、司法性质的文件，及其官方正式译文；
+（二）单纯事实消息；
+（三）历法、通用数表、通用表格和公式。
+"""
+
+let Contributors: [String] = [
+    "@文涛",
+    "@nuomi1",
+    "@allinprogram",
+    "@andmeics",
+    "@JalinWang",
+]
+
+let DeveloperMail = "rankki.dev@icloud.com"
